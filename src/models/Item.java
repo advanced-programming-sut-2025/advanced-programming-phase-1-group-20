7f214dff2dbbf275da17e904081bfc3128f3c1db
kasra <kasra.sh1385@gmail.com>
2025-04-19 17:00:15 +0330

    making new packages

--- src/models/Item.java
+++ src/models/Item.java
@@ -2,7 +2,12 @@ package models;
 
 public class Item {
     String name;
+    int price;
     public Item(String name, int price){
         this.name = name;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
     }
 }
