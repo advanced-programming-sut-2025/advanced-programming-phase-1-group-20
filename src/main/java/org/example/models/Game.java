8874fe69bb2ca8fbfa7e442d7f8fb71de830187b
Kasra <kasra.sh1385@gmail.com>
2025-04-22 11:41:01 +0330

    added a method

--- src/main/java/org/example/models/Game.java
+++ src/main/java/org/example/models/Game.java
@@ -11,11 +11,15 @@ public class Game {
 
     public Game(List<Player> players) {
         this.players = players;
-        this.currentPlayer = players.get(0); // Set the first player as the current player
+        this.currentPlayer = players.get(0); // Set the first player as the current player\
         this.date = new Date();
     }
 
     public Date getDate() {
         return date;
+    }
+
+    public Player getCurrentPlayer() {
+        return currentPlayer;
     }
 }
