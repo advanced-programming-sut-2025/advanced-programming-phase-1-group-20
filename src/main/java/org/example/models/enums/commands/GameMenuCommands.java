39a5168541d94cefc066cf781c8bdb2ded05baf6
Kasra <kasra.sh1385@gmail.com>
2025-04-22 19:10:49 +0330

    weather commands

--- src/main/java/org/example/models/enums/commands/GameMenuCommands.java
+++ src/main/java/org/example/models/enums/commands/GameMenuCommands.java
@@ -3,18 +3,27 @@ package org.example.models.enums.commands;
 import java.util.regex.Pattern;
 
 public enum GameMenuCommands implements Command {
+
+    // time related
     ShowTime(Pattern.compile("^time$")),
     ShowDate(Pattern.compile("^date$")),
-    ShowDateTime(Pattern.compile("^clock$")),
-    SaveGame(Pattern.compile("^save$")),
-    AdvanceTime(Pattern.compile("^cheat\\s+advance\\s+time\\s+(\\d+)$")), // cheat command
-    AdvanceDate(Pattern.compile("^cheat\\s+advance\\s+date\\s+(\\d+)$")), // cheat command
-    Move(Pattern.compile("^move\\s+(\\d+)\\s+(\\d+)$")),
-    DayOfWeek(Pattern.compile("^day\\s+of\\s+week$")),
+    ShowDateTime(Pattern.compile("^clock$|^datetime$")),
+    AdvanceTime(Pattern.compile("^cheat\\s+advance\\s+time\\s+(\\d+)h?$")), // cheat command
+    AdvanceDate(Pattern.compile("^cheat\\s+advance\\s+date\\s+(\\d+)d?$")), // cheat command
+    DayOfWeek(Pattern.compile("^day\\s+of\\s+(the\\s+)?week$")),
     ShowInventory(Pattern.compile("^inventory$")),
     ShowLocation(Pattern.compile("^location$")),
     ShowEnergy(Pattern.compile("^energy$")),
     ShowMap(Pattern.compile("^map$")),
+
+    // weather related
+    ShowSeason(Pattern.compile("^season$")),
+    ShowWeather(Pattern.compile("^weather$")),
+    ShowWeatherForecast(Pattern.compile("^weather\\s+forecast$")),
+    SetWeather(Pattern.compile("^cheat\\s+weather\\s+set\\s+(\\w+)$")), // cheat code
+    CheatThor(Pattern.compile("^cheat\\s+Thor\\s+-l\\s+([\\d\\s,]+)$")), // cheat code
+    Move(Pattern.compile("^move\\s+(\\d+)\\s+(\\d+)$")),
+    SaveGame(Pattern.compile("^save$")),
     AutoSave(Pattern.compile("^autosave$")),
 
     // TODO: add more commands
