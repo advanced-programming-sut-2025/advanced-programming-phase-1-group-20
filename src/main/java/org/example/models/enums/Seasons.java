5583bd9d9d39eef5dddbb012b659c18da6493b43
MostafaOmidi17 <mostafaomidi2006@gmail.com>
2025-04-26 14:31:02 +0330

    added mixed seeds to seasons

--- src/main/java/org/example/models/enums/Seasons.java
+++ src/main/java/org/example/models/enums/Seasons.java
@@ -1,5 +1,25 @@
 package org.example.models.enums;
 
+import org.example.models.App;
+import org.example.models.Items.Seed;
+
+import java.util.Arrays;
+import java.util.List;
+
 public enum Seasons {
-    SPRING, SUMMER, AUTUMN, WINTER;
+    SPRING(new String[]{"Cauliflower", "Parsnip", "Potato", "Blue Jazz", "Tulip"}),
+    SUMMER(new String[]{"Corn", "Hot Pepper" , "Radish" , "Wheat", "Poppy", "Sunflower" , "Summer Spangle"}),
+    AUTUMN(new String[]{"Artichoke" , "Corn" , "Eggplant" , "Pumpkin", "Sunflower" , "Fairy Rose"}),
+    WINTER(new String[]{"Powdermelon"});
+    private final String[] mixedSeeds;
+
+    Seasons(String[] seeds) {
+        this.mixedSeeds = seeds;
+    }
+
+
+    public String[] getSeeds() {
+        return mixedSeeds;
+    }
+
 }
