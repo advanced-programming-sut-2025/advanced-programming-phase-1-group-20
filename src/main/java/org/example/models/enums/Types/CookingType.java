1b7e10e24aeb412e146587ae2aeb918351c948a5
MostafaOmidi17 <mostafaomidi2006@gmail.com>
2025-05-11 14:16:39 +0330

    markets except fish market is completed

--- src/main/java/org/example/models/enums/Types/CookingType.java
+++ src/main/java/org/example/models/enums/Types/CookingType.java
@@ -13,6 +13,19 @@ public enum CookingType {
     Tortilla("Tortilla", Ingredients.Tortilla, 50, "", "Stardrop Saloon", 50),
     MakiRoll("Maki Roll", Ingredients.MakiRoll, 100, "", "Stardrop Saloon", 220),
     TripleShotEspresso("Triple Shot Espresso", Ingredients.TripleShotEspresso, 200, "Max Energy + 100 (5 hours)", "Stardrop Saloon", 450),
+    Cookie("Cookie", Ingredients.Cookie, 90, "" , "Stardrop Saloon" , 140),
+    HashBrowns("hash browns", Ingredients.hashbrowns, 90, "Farming (5 hours)" , "Stardrop Saloon" ,  120),
+    Pancakes("pancakes", Ingredients.pancakes, 90,"Foraging (11 hours)" , "Stardrop Saloon" ,  80),
+    FruitSalad("fruit salad", Ingredients.fruitsalad, 263, "" , "Stardrop Saloon" ,  450),
+    RedPlate("red plate", Ingredients.redplate, 240, "max Energy +50 (3 hours)" , "Stardrop Saloon" , 400),
+    Bread("bread", Ingredients.bread, 50,"" , "Stardrop Saloon" , 60),
+    SalmonDinner("salmon dinner", Ingredients.salmondinner, 125, "" , "Leah reward" , 300),
+    VegetableMedley("vegetable medley", Ingredients.vegetablemedley, 165,"" , "Foraging Level 2" , 120),
+    FarmersLunch("farmer's lunch", Ingredients.farmerslunch, 200, "Farming (5 hours)" , "Farming level 1" , 150),
+    SurvivalBurger("survival burger", Ingredients.survivalburger, 125, "Foraging (5 hours)" , "Foraging level 3" , 180),
+    DishOTheSea("dish O' the Sea", Ingredients.dishOtheSea, 150, "Fishing (5 hours)" , "Fishing level 2" , 220),
+    SeaFormPudding("seaform Pudding", Ingredients.seaformPudding, 175,"Fishing (10 hours)" , "Fishing level 3" ,  300),
+    MinersTreat("miner's treat", Ingredients.minerstreat, 125, "Mining (5 hours)" , "Mining level 1" , 200),
     ;
     private final String name;
     private final Ingredients ingredients;
