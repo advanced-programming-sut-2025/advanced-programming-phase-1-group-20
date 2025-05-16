f3a8fa4b98a54c896c542326c703897f4a310f3a
MostafaOmidi17 <mostafaomidi2006@gmail.com>
2025-05-16 11:44:49 +0330

    hmmm

--- src/main/java/org/example/models/Player/Backpack.java
+++ src/main/java/org/example/models/Player/Backpack.java
@@ -16,7 +16,6 @@ public class Backpack {
     }
 
     public boolean add(Item item, int quantity) {
-        System.out.println("hmm");
         if (type == Type.Initial) {
             if (countItems() == 12) {
                 return false;
