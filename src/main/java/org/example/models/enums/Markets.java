083a6d1eee9f8ab167abe8fd8f76d6b528e32131
MostafaOmidi17 <mostafaomidi2006@gmail.com>
2025-05-17 08:54:08 +0330

    Marnie Shop completed successfully (animals and tools)

--- src/main/java/org/example/models/enums/Markets.java
+++ src/main/java/org/example/models/enums/Markets.java
@@ -3,6 +3,8 @@ package org.example.models.enums;
 import org.example.models.Items.*;
 import org.example.models.Market;
 import org.example.models.entities.animal.Animal;
+import org.example.models.entities.animal.BarnAnimal;
+import org.example.models.entities.animal.CoopAnimal;
 import org.example.models.enums.PlayerEnums.Skills;
 import org.example.models.enums.Types.*;
 
@@ -663,28 +665,36 @@ public enum Markets {
         Item milkPail = new Tool("Milk Pail" , 1_000 , "Gather milk from your animals." , Tool.ToolType.MILK_PAIL , Tool.ToolMaterial.BASIC , 5 , Skills.FARMING );
         items.put(milkPail, 1.0);
 
-        Item chicken = new Animal("Chicken" , 800);
+        Item chicken = new CoopAnimal(org.example.models.enums.CoopAnimalTypes.CHICKEN);
+        chicken.setPrice(800);
         items.put(chicken, 2.0);
 
-        Item cow = new Animal("Cow" , 1_500);
+        Item cow = new BarnAnimal(BarnAnimalTypes.COW , "Cow");
+        cow.setPrice(1_500);
         items.put(cow, 2.0);
 
-        Item goat = new Animal("Goat" , 4_000);
+        Item goat = new BarnAnimal( BarnAnimalTypes.GOAT , "Goat");
+        goat.setPrice(4_000);
         items.put(goat, 2.0);
 
-        Item duck = new Animal("Duck" , 1_200);
+        Item duck = new CoopAnimal(org.example.models.enums.CoopAnimalTypes.DUCK);
+        duck.setPrice(1_200);
         items.put(duck, 2.0);
 
-        Item sheep = new Animal("Sheep" , 8_000);
+        Item sheep = new BarnAnimal( BarnAnimalTypes.SHEEP, "Sheep");
+        sheep.setPrice(8_000);
         items.put(sheep, 2.0);
 
-        Item rabbit = new Animal("Rabbit" , 8_500);
+        Item rabbit = new CoopAnimal(org.example.models.enums.CoopAnimalTypes.RABBIT);
+        rabbit.setPrice(8_500);
         items.put(rabbit, 2.0);
 
-        Item dinosaur = new Animal("Dinosaur" , 14_000);
+        Item dinosaur = new CoopAnimal(org.example.models.enums.CoopAnimalTypes.DINOSAUR);
+        dinosaur.setPrice(14_000);
         items.put(dinosaur, 2.0);
 
-        Item pig = new Animal("Pig" , 16_000);
+        Item pig = new BarnAnimal(BarnAnimalTypes.PIG, "Pig");
+        pig.setPrice(16_000);
         items.put(pig, 2.0);
 
 
