0dd282f1deb81bf31ddaadc418e6740ce1c4e3bd
Taha <taha.samadi@gmail.com>
2025-07-12 15:31:25 +0330

    menu...

--- core/src/main/java/org/example/controllers/WelcomeMenuController.java
+++ core/src/main/java/org/example/controllers/WelcomeMenuController.java
@@ -1,4 +1,39 @@
 package org.example.controllers;
 
+import com.badlogic.gdx.graphics.Texture;
+import org.example.utils.GameAssetManager;
+import org.example.views.WelcomeMenuScreen;
+
 public class WelcomeMenuController {
+    private final WelcomeMenuScreen screen;
+    private final GameAssetManager assetManager;
+
+    private int currentImageIndex = 0;
+    private float timeSinceLastChange = 0;
+    private static final float IMAGE_CHANGE_INTERVAL = 0.1f;
+
+    public WelcomeMenuController(WelcomeMenuScreen screen) {
+        this.screen = screen;
+        this.assetManager = GameAssetManager.getGameAssetManager();
+    }
+
+    public void update(float delta) {
+        timeSinceLastChange += delta;
+        if (timeSinceLastChange >= IMAGE_CHANGE_INTERVAL) {
+            timeSinceLastChange = 0;
+            currentImageIndex = (currentImageIndex + 1) % assetManager.getWelcomeMenuImagesCount();
+            updateBackground();
+        }
+    }
+
+    private void updateBackground() {
+        Texture currentTexture = assetManager.getWelcomeMenuTexture(currentImageIndex);
+        screen.updateBackground(currentTexture);
+    }
+
+    public void handleLoginButton() {
+    }
+
+    public void handleSignUpButton() {
+    }
 }
--- core/src/main/java/org/example/views/WelcomeMenuScreen.java
+++ core/src/main/java/org/example/views/WelcomeMenuScreen.java
@@ -1,4 +1,107 @@
 package org.example.views;
 
-public class WelcomeMenuScreen {
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.*;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import org.example.controllers.WelcomeMenuController;
+import org.example.utils.GameAssetManager;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+
+public class WelcomeMenuScreen implements Screen {
+    private TextButton signUpButton;
+    private TextButton loginButton;
+    private TextButton exitButton;
+    private final WelcomeMenuController controller;
+    private final Stage stage;
+    private final Skin skin;
+    private Image background;
+
+    public WelcomeMenuScreen(WelcomeMenuController controller, Skin skin) {
+        this.controller = controller;
+        this.skin = skin;
+        this.stage = new Stage(new ScreenViewport());
+        setupUI();
+    }
+
+    private void setupUI() {
+        background = new Image();
+        background.setFillParent(true);
+        stage.addActor(background);
+
+        Table table = new Table();
+        table.setFillParent(true);
+        table.bottom().padBottom(180);
+
+        signUpButton = new TextButton("SIGN UP", skin);
+        loginButton = new TextButton("LOG IN", skin);
+        exitButton = new TextButton("EXIT", skin);
+
+        table.add(signUpButton).pad(10);
+        table.add(loginButton).pad(10);
+        table.row();
+        table.add(exitButton).colspan(2).center().padTop(10);
+
+        stage.addActor(table);
+
+        loginButton.addListener(new ChangeListener() {
+            public void changed(ChangeEvent event, Actor actor) {
+                //GameAssetManager.getGameAssetManager().playClickSound();
+                controller.handleLoginButton();
+            }
+        });
+
+        signUpButton.addListener(new ChangeListener() {
+            public void changed(ChangeEvent event, Actor actor) {
+                //GameAssetManager.getGameAssetManager().playClickSound();
+                controller.handleSignUpButton();
+            }
+        });
+
+        exitButton.addListener(new ChangeListener() {
+            public void changed(ChangeEvent event, Actor actor) {
+                //GameAssetManager.getGameAssetManager().playClickSound();
+                Gdx.app.exit();
+            }
+        });
+    }
+
+    public void updateBackground(Texture texture) {
+        background.setDrawable(new TextureRegionDrawable(new TextureRegion(texture)));
+    }
+
+    public void render(float delta) {
+        controller.update(delta);
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        stage.act(delta);
+        stage.draw();
+    }
+
+    public void show() {
+        Gdx.input.setInputProcessor(stage);
+        Pixmap pixmap = new Pixmap(Gdx.files.internal("all/cursor.png"));
+        Gdx.graphics.setCursor(Gdx.graphics.newCursor(pixmap, 0, 0));
+        pixmap.dispose();
+    }
+
+    public void resize(int width, int height) {
+        stage.getViewport().update(width, height, true);
+    }
+
+    public void pause() {}
+    public void resume() {}
+    public void hide() {}
+
+    public void dispose() {
+        stage.dispose();
+        GameAssetManager.getGameAssetManager().disposeWelcomeMenuTextures();
+    }
 }
