5de1a1204d0dc11d40559120081a38f712120e59
Taha <taha.samadi@gmail.com>
2025-07-12 15:19:04 +0330

    add gif dependency

--- core/src/main/java/org/example/utils/GameAssetManager.java
+++ core/src/main/java/org/example/utils/GameAssetManager.java
@@ -1,24 +1,34 @@
 package org.example.utils;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 
 public class GameAssetManager {
     private static GameAssetManager gameAssetManager;
     private final Skin skin = new Skin(Gdx.files.internal("ui/uiskin.json"));
-
+    private final Texture[] welcomeMenuImages = new Texture[20];
 
     public static GameAssetManager getGameAssetManager(){
-        if(gameAssetManager == null){
+        if (gameAssetManager == null) {
             gameAssetManager = new GameAssetManager();
         }
         return gameAssetManager;
     }
 
+    public void loadWelcomeMenuTextures() {
+        for (int i = 0; i < 20; i++) {
+            welcomeMenuImages[i] = new Texture(Gdx.files.internal("WelcomeMenu/" + (i + 1) + ".png"));
+        }
+    }
+
+    public Texture getWelcomeMenuTexture(int index) {
+        return welcomeMenuImages[index - 1];
+    }
+
     public Skin getSkin() {
         return skin;
     }
-
 
 
 }
