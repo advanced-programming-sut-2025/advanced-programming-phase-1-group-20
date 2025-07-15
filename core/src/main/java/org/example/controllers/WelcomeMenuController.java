2ea4441101ab97abfa8d07b27adc32d281e150a1
Taha <taha.samadi@gmail.com>
2025-07-15 14:24:01 +0330

    menu...

--- core/src/main/java/org/example/controllers/WelcomeMenuController.java
+++ core/src/main/java/org/example/controllers/WelcomeMenuController.java
@@ -61,43 +61,43 @@ public class WelcomeMenuController {
         registerScreen.showRegisterFormDirectly();
     }
 
-    public void handleTryGameButton() {
-        Main.getGame().getScreen().dispose();
-
-        User user1 = new User("guest user1" , "1234" , "guest@gmail.com" , "guest" , Gender.Male);
-        Player player1 = new Player(user1);
-        User user2 = new User("guest user1" , "1234" , "guest@gmail.com" , "guest" , Gender.Male);
-        Player player2 = new Player(user2);
-        User user3 = new User("guest user1" , "1234" , "guest@gmail.com" , "guest" , Gender.Male);
-        Player player3 = new Player(user3);
-        User user4 = new User("guest user1" , "1234" , "guest@gmail.com" , "guest" , Gender.Male);
-        Player player4 = new Player(user4);
-
-        List<Player> players = new ArrayList<>();
-        players.add(player1);
-        players.add(player2);
-        players.add(player3);
-        players.add(player4);
-
-        Game game = new Game(players, player1);
-        App.setGame(game);
-
-        GameMap map = new GameMap();
-        Farm farm1 = new Farm("guest farm" , player1 , true , 1);
-        player1.setCurrentFarm(farm1);
-        map.addFarm(farm1);
-        Farm farm2 = new Farm("guest farm" , player2 , false , 2);
-        player2.setCurrentFarm(farm2);
-        map.addFarm(farm2);
-        Farm farm3 = new Farm("guest farm" , player3 , true , 3);
-        player3.setCurrentFarm(farm3);
-        map.addFarm(farm3);
-        Farm farm4 = new Farm("guest farm" , player4 , false , 4);
-        player4.setCurrentFarm(farm4);
-        map.addFarm(farm4);
-
-        game.setGameMap(map);
-        GameMenuScreen gameMenuScreen = new GameMenuScreen(new GameMenuController(player1) , player1 , game , assetManager.getSkin() ,user1);
-        Main.getGame().setScreen(gameMenuScreen);
-    }
+//    public void handleTryGameButton() {
+//        Main.getGame().getScreen().dispose();
+//
+//        User user1 = new User("guest user1" , "1234" , "guest@gmail.com" , "guest" , Gender.Male);
+//        Player player1 = new Player(user1);
+//        User user2 = new User("guest user1" , "1234" , "guest@gmail.com" , "guest" , Gender.Male);
+//        Player player2 = new Player(user2);
+//        User user3 = new User("guest user1" , "1234" , "guest@gmail.com" , "guest" , Gender.Male);
+//        Player player3 = new Player(user3);
+//        User user4 = new User("guest user1" , "1234" , "guest@gmail.com" , "guest" , Gender.Male);
+//        Player player4 = new Player(user4);
+//
+//        List<Player> players = new ArrayList<>();
+//        players.add(player1);
+//        players.add(player2);
+//        players.add(player3);
+//        players.add(player4);
+//
+//        Game game = new Game(players, player1);
+//        App.setGame(game);
+//
+//        GameMap map = new GameMap();
+//        Farm farm1 = new Farm("guest farm" , player1 , true , 1);
+//        player1.setCurrentFarm(farm1);
+//        map.addFarm(farm1);
+//        Farm farm2 = new Farm("guest farm" , player2 , false , 2);
+//        player2.setCurrentFarm(farm2);
+//        map.addFarm(farm2);
+//        Farm farm3 = new Farm("guest farm" , player3 , true , 3);
+//        player3.setCurrentFarm(farm3);
+//        map.addFarm(farm3);
+//        Farm farm4 = new Farm("guest farm" , player4 , false , 4);
+//        player4.setCurrentFarm(farm4);
+//        map.addFarm(farm4);
+//
+//        game.setGameMap(map);
+//        GameMenuScreen gameMenuScreen = new GameMenuScreen(new GameMenuController(player1) , player1 , game , assetManager.getSkin() ,user1);
+//        Main.getGame().setScreen(gameMenuScreen);
+//    }
 }
