e8403613e592de6361f21d7b2f8248022b4f78dc
Kasra <kasra.sh1385@gmail.com>
2025-07-15 15:44:10 +0330

    walkable

--- core/src/main/java/org/example/models/enums/Types/TileType.java
+++ core/src/main/java/org/example/models/enums/Types/TileType.java
@@ -1,33 +1,38 @@
 package org.example.models.enums.Types;
 
 public enum TileType {
-    WATER,
-    COBBLED_STONE,
-    GRASS,
-    SAND,
-    STONE,
-    TREE,
-    IRON_ORE,
-    BARN,
-    COOP,
-    PLOWED,
-    CROP,
-    GOLD_ORE,
-    DIAMOND_ORE,
-    VILLAGE,
-    MARKET,
-    PATH,
-    BUILDING,
-    LAKE,
-    QUARRY,
-    SHIPPING_BIN,
-    EMERALD_ORE,
-    CONSTRUCTED_GREENHOUSE,
-    BRANCH,
-    GREENHOUSE;
+    WATER(false),
+    COBBLED_STONE(false),
+    GRASS(true),
+    SAND(true),
+    STONE(true),
+    TREE(false),
+    IRON_ORE(false),
+    BARN(false),
+    COOP(false),
+    PLOWED(true),
+    CROP(true),
+    GOLD_ORE(false),
+    DIAMOND_ORE(false),
+    VILLAGE(false),
+    MARKET(false),
+    PATH(true),
+    BUILDING(false),
+    LAKE(false),
+    QUARRY(false),
+    SHIPPING_BIN(false),
+    EMERALD_ORE(false),
+    CONSTRUCTED_GREENHOUSE(false),
+    BRANCH(true),
+    GREENHOUSE(false);
 
-    final boolean isWalkable;
+    private final boolean isWalkable;
+
     TileType(boolean isWalkable) {
         this.isWalkable = isWalkable;
+    }
+
+    public boolean isWalkable() {
+        return isWalkable;
     }
 }
